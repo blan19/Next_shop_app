481f3416e0c755173a03011e98b165bf16941ac3
JunSeo <oponize@naver.com>
2022-03-07 19:42:31 +0900

    add admin page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import Responsive from '@/utils/styles/Responsive';
 import Layouts from '../Layouts';
+import { withIronSessionSsr } from 'iron-session/next';
+import { sessionOptions } from '@/utils/iron/session';
+import { User } from './api/user';
+import { InferGetServerSidePropsType } from 'next';
 
-const Admin = () => {
-  const [mounted, setMounted] = useState(true);
+const Admin = ({
+  user,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (!user.isLoggedIn) {
+      router.push('/');
+    }
+    if (!user.admin) {
+      router.push('/');
+    }
+  }, [router, user.admin, user.isLoggedIn]);
   return (
     <Layouts>
       <AdminResponsive>
@@ -18,3 +32,30 @@ const Admin = () => {
 export default Admin;
 
 const AdminResponsive = styled(Responsive)``;
+
+export const getServerSideProps = withIronSessionSsr(async function ({
+  req,
+  res,
+}) {
+  const user = req.session.user;
+  if (user === undefined) {
+    res.setHeader('location', '/login');
+    res.statusCode = 302;
+    res.end();
+    return {
+      props: {
+        user: { isLoggedIn: false, email: '', uid: '', admin: false },
+      },
+    };
+  }
+  if (user.email === process.env.ADMIN_EMAIL) {
+    return {
+      props: { user: { ...req.session.user, admin: true } },
+    };
+  } else {
+    return {
+      props: { user: { ...req.session.user, admin: false } },
+    };
+  }
+},
+sessionOptions);
