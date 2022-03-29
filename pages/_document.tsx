d1e2ca20b4371fd796abe07062b7a480231cdfd8
Junseo <oponize@naver.com>
2022-03-29 10:04:55 +0900

    Build: add search console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -76,15 +76,11 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-          <html lang="ko" />
           <meta
             name="google-site-verification"
-            content="vw1yNIFT-LCDTHTlmQHCzmE08nVjABQ9SPmzBiclhlU"
+            content="Py_afcHdJUFmCzWGXB-EmHItssuvcAhBGPpHjCoa05c"
           />
-          <meta
-            name="naver-site-verification"
-            content="00e9558b544b2b7c9b2941ff2225aff2b8a2c8a2"
-          />
+          <html lang="ko" />
         </Head>
         <body>
           <script dangerouslySetInnerHTML={{ __html: noflash }} />
